5edabd6378381a71c1b9f987ab01cb8cd908bd80
Patrick J. Burns <patrick@diyclassics.org>
2018-10-11 16:33:39 -0400

    Fix stop tests for Classical Chinese

--- cltk/tests/test_nlp/test_stop.py
+++ cltk/tests/test_nlp/test_stop.py
@@ -124,18 +124,19 @@ class TestStop_General(unittest.TestCase):
         """
         Set up sample texts/corpus for testing stop.py
         """
-        self.test_1 = """cogitanti mihi saepe numero et memoria vetera repetenti perbeati fuisse, quinte frater, illi videri solent, qui in optima re publica, cum et honoribus et rerum gestarum gloria florerent, eum vitae cursum tenere potuerunt, ut vel in negotio sine periculo vel in otio cum dignitate esse possent; ac fuit cum mihi quoque initium requiescendi atque animum ad utriusque nostrum praeclara studia referendi fore iustum et prope ab omnibus concessum arbitrarer, si infinitus forensium rerum labor et ambitionis occupatio decursu honorum, etiam aetatis flexu constitisset. quam spem cogitationum et consiliorum meorum cum graves communium temporum tum varii nostri casus fefellerunt; nam qui locus quietis et tranquillitatis plenissimus fore videbatur, in eo maximae moles molestiarum et turbulentissimae tempestates exstiterunt; neque vero nobis cupientibus atque exoptantibus fructus oti datus est ad eas artis, quibus a pueris dediti fuimus, celebrandas inter nosque recolendas. nam prima aetate incidimus in ipsam perturbationem disciplinae veteris, et consulatu devenimus in medium rerum omnium certamen atque discrimen, et hoc tempus omne post consulatum obiecimus eis fluctibus, qui per nos a communi peste depulsi in nosmet ipsos redundarent. sed tamen in his vel asperitatibus rerum vel angustiis temporis obsequar studiis nostris et quantum mihi vel fraus inimicorum vel causae amicorum vel res publica tribuet oti, ad scribendum potissimum conferam; tibi vero, frater, neque hortanti deero neque roganti, nam neque auctoritate quisquam apud me plus valere te potest neque voluntate."""
 
-        self.test_2 = """ac mihi repetenda est veteris cuiusdam memoriae non sane satis explicata recordatio, sed, ut arbitror, apta ad id, quod requiris, ut cognoscas quae viri omnium eloquentissimi clarissimique senserint de omni ratione dicendi. vis enim, ut mihi saepe dixisti, quoniam, quae pueris aut adulescentulis nobis ex commentariolis nostris incohata ac rudia exciderunt, vix sunt hac aetate digna et hoc usu, quem ex causis, quas diximus, tot tantisque consecuti sumus, aliquid eisdem de rebus politius a nobis perfectiusque proferri; solesque non numquam hac de re a me in disputationibus nostris dissentire, quod ego eruditissimorum hominum artibus eloquentiam contineri statuam, tu autem illam ab elegantia doctrinae segregandam putes et in quodam ingeni atque exercitationis genere ponendam. ac mihi quidem saepe numero in summos homines ac summis ingeniis praeditos intuenti quaerendum esse visum est quid esset cur plures in omnibus rebus quam in dicendo admirabiles exstitissent; nam quocumque te animo et cogitatione converteris, permultos excellentis in quoque genere videbis non mediocrium artium, sed prope maximarum. quis enim est qui, si clarorum hominum scientiam rerum gestarum vel utilitate vel magnitudine metiri velit, non anteponat oratori imperatorem? quis autem dubitet quin belli duces ex hac una civitate praestantissimos paene innumerabilis, in dicendo autem excellentis vix paucos proferre possimus? iam vero consilio ac sapientia qui regere ac gubernare rem publicam possint, multi nostra, plures patrum memoria atque etiam maiorum exstiterunt, cum boni perdiu nulli, vix autem singulis aetatibus singuli tolerabiles oratores invenirentur. ac ne qui forte cum aliis studiis, quae reconditis in artibus atque in quadam varietate litterarum versentur, magis hanc dicendi rationem, quam cum imperatoris laude aut cum boni senatoris prudentia comparandam putet, convertat animum ad ea ipsa artium genera circumspiciatque, qui in eis floruerint quamque multi sint; sic facillime, quanta oratorum sit et semper fuerit paucitas, iudicabit."""
+        # These is not a 'general' tests; should be moved
+        self.latin_1 = """cogitanti mihi saepe numero et memoria vetera repetenti perbeati fuisse, quinte frater, illi videri solent, qui in optima re publica, cum et honoribus et rerum gestarum gloria florerent, eum vitae cursum tenere potuerunt, ut vel in negotio sine periculo vel in otio cum dignitate esse possent; ac fuit cum mihi quoque initium requiescendi atque animum ad utriusque nostrum praeclara studia referendi fore iustum et prope ab omnibus concessum arbitrarer, si infinitus forensium rerum labor et ambitionis occupatio decursu honorum, etiam aetatis flexu constitisset. quam spem cogitationum et consiliorum meorum cum graves communium temporum tum varii nostri casus fefellerunt; nam qui locus quietis et tranquillitatis plenissimus fore videbatur, in eo maximae moles molestiarum et turbulentissimae tempestates exstiterunt; neque vero nobis cupientibus atque exoptantibus fructus oti datus est ad eas artis, quibus a pueris dediti fuimus, celebrandas inter nosque recolendas. nam prima aetate incidimus in ipsam perturbationem disciplinae veteris, et consulatu devenimus in medium rerum omnium certamen atque discrimen, et hoc tempus omne post consulatum obiecimus eis fluctibus, qui per nos a communi peste depulsi in nosmet ipsos redundarent. sed tamen in his vel asperitatibus rerum vel angustiis temporis obsequar studiis nostris et quantum mihi vel fraus inimicorum vel causae amicorum vel res publica tribuet oti, ad scribendum potissimum conferam; tibi vero, frater, neque hortanti deero neque roganti, nam neque auctoritate quisquam apud me plus valere te potest neque voluntate."""
 
-        self.test_corpus = [self.test_1, self.test_2]
+        self.latin_2 = """ac mihi repetenda est veteris cuiusdam memoriae non sane satis explicata recordatio, sed, ut arbitror, apta ad id, quod requiris, ut cognoscas quae viri omnium eloquentissimi clarissimique senserint de omni ratione dicendi. vis enim, ut mihi saepe dixisti, quoniam, quae pueris aut adulescentulis nobis ex commentariolis nostris incohata ac rudia exciderunt, vix sunt hac aetate digna et hoc usu, quem ex causis, quas diximus, tot tantisque consecuti sumus, aliquid eisdem de rebus politius a nobis perfectiusque proferri; solesque non numquam hac de re a me in disputationibus nostris dissentire, quod ego eruditissimorum hominum artibus eloquentiam contineri statuam, tu autem illam ab elegantia doctrinae segregandam putes et in quodam ingeni atque exercitationis genere ponendam. ac mihi quidem saepe numero in summos homines ac summis ingeniis praeditos intuenti quaerendum esse visum est quid esset cur plures in omnibus rebus quam in dicendo admirabiles exstitissent; nam quocumque te animo et cogitatione converteris, permultos excellentis in quoque genere videbis non mediocrium artium, sed prope maximarum. quis enim est qui, si clarorum hominum scientiam rerum gestarum vel utilitate vel magnitudine metiri velit, non anteponat oratori imperatorem? quis autem dubitet quin belli duces ex hac una civitate praestantissimos paene innumerabilis, in dicendo autem excellentis vix paucos proferre possimus? iam vero consilio ac sapientia qui regere ac gubernare rem publicam possint, multi nostra, plures patrum memoria atque etiam maiorum exstiterunt, cum boni perdiu nulli, vix autem singulis aetatibus singuli tolerabiles oratores invenirentur. ac ne qui forte cum aliis studiis, quae reconditis in artibus atque in quadam varietate litterarum versentur, magis hanc dicendi rationem, quam cum imperatoris laude aut cum boni senatoris prudentia comparandam putet, convertat animum ad ea ipsa artium genera circumspiciatque, qui in eis floruerint quamque multi sint; sic facillime, quanta oratorum sit et semper fuerit paucitas, iudicabit."""
+
+        self.test_latin_corpus = [self.latin_1, self.latin_2]
 
     def test_corpus_stop_list_freq(self):
         """Test production of stoplists from a corpus, using basis: frequency"""
         target_list = ['ac', 'ad', 'atque', 'cum', 'et', 'in', 'mihi', 'neque', 'qui', 'vel']
-
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='frequency', inc_values=False)
         self.assertEqual(stoplist, target_list)
 
@@ -146,7 +147,7 @@ class TestStop_General(unittest.TestCase):
                         ('et', 15), ('in', 18), ('mihi', 6), ('neque', 5),
                         ('qui', 7), ('vel', 9)]
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='frequency', inc_values=True)
         self.assertEqual(stoplist, target_list)
 
@@ -155,7 +156,7 @@ class TestStop_General(unittest.TestCase):
         target_list = ['ac', 'atque', 'autem', 'cum', 'et', 'in', 'mihi', 'neque', 'qui', 'vel']
 
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='tfidf', inc_values=False)
         self.assertEqual(stoplist, target_list)
 
@@ -164,7 +165,7 @@ class TestStop_General(unittest.TestCase):
         target_list = ['ac', 'atque', 'cum', 'et', 'in', 'mihi', 'neque', 'qui', 'vel']
 
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='frequency', inc_values=False, exclude=['ad'])
         self.assertEqual(stoplist, target_list)
 
@@ -172,7 +173,7 @@ class TestStop_General(unittest.TestCase):
         """Test production of stoplists from a corpus, using basis: frequency"""
         target_list = ['ac', 'ad', 'atque', 'cum', 'est', 'et', 'in', 'mihi', 'neque', 'qui', 'vel']
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10, basis='frequency', include=['est'])
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10, basis='frequency', include=['est'])
         self.assertEqual(stoplist, target_list)
 
     def test_corpus_stop_list_freq_sort_words(self):
@@ -180,7 +181,7 @@ class TestStop_General(unittest.TestCase):
         target_list = ['in', 'et', 'vel', 'ac', 'cum', 'qui', 'atque', 'mihi', 'ad', 'neque']
 
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='frequency', inc_values=False, sort_words=False)
         self.assertEqual(stoplist, target_list)
 
@@ -189,7 +190,7 @@ class TestStop_General(unittest.TestCase):
         target_list = ['ac', 'atque', 'cum', 'et', 'in', 'mihi', 'neque', 'qui', 'rerum', 'vel']
 
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='mean', inc_values=False)
         self.assertEqual(stoplist, target_list)
 
@@ -197,7 +198,7 @@ class TestStop_General(unittest.TestCase):
         """Test production of stoplists from a corpus, using basis: variance"""
         target_list = ['ac', 'atque', 'cum', 'et', 'in', 'mihi', 'neque', 'qui', 'rerum', 'vel']
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='variance', inc_values=False)
         self.assertEqual(stoplist, target_list)
 
@@ -205,14 +206,14 @@ class TestStop_General(unittest.TestCase):
         """Test production of stoplists from a corpus, using basis: variance"""
         target_list = ['ac', 'atque', 'cum', 'et', 'in', 'mihi', 'neque', 'qui', 'rerum', 'vel']
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,basis='variance')
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,basis='variance')
         self.assertEqual(stoplist, target_list)
 
     def test_corpus_stop_list_entropy(self):
         """Test production of stoplists from a corpus, using basis: entropy"""
         target_list = ['ac', 'ad', 'atque', 'cum', 'et', 'in', 'mihi', 'qui', 'rerum', 'vel']
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='entropy', inc_values=False)
         self.assertEqual(stoplist, target_list)
 
@@ -221,7 +222,7 @@ class TestStop_General(unittest.TestCase):
         target_list = ['ac', 'atque', 'cum', 'et', 'in', 'mihi', 'neque',
                         'qui', 'rerum', 'vel']
         S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.test_corpus, size=10,
+        stoplist = S.build_stoplist(self.test_latin_corpus, size=10,
                     basis='zou', inc_values=False)
         self.assertEqual(stoplist, target_list)
 
@@ -236,20 +237,18 @@ class TestStop_LanguageSpecific(unittest.TestCase):
         """
         Set up sample texts/corpus for testing stop.py
         """
-        self.test_1 = """cogitanti mihi saepe numero et memoria vetera repetenti perbeati fuisse, quinte frater, illi videri solent, qui in optima re publica, cum et honoribus et rerum gestarum gloria florerent, eum vitae cursum tenere potuerunt, ut vel in negotio sine periculo vel in otio cum dignitate esse possent; ac fuit cum mihi quoque initium requiescendi atque animum ad utriusque nostrum praeclara studia referendi fore iustum et prope ab omnibus concessum arbitrarer, si infinitus forensium rerum labor et ambitionis occupatio decursu honorum, etiam aetatis flexu constitisset. quam spem cogitationum et consiliorum meorum cum graves communium temporum tum varii nostri casus fefellerunt; nam qui locus quietis et tranquillitatis plenissimus fore videbatur, in eo maximae moles molestiarum et turbulentissimae tempestates exstiterunt; neque vero nobis cupientibus atque exoptantibus fructus oti datus est ad eas artis, quibus a pueris dediti fuimus, celebrandas inter nosque recolendas. nam prima aetate incidimus in ipsam perturbationem disciplinae veteris, et consulatu devenimus in medium rerum omnium certamen atque discrimen, et hoc tempus omne post consulatum obiecimus eis fluctibus, qui per nos a communi peste depulsi in nosmet ipsos redundarent. sed tamen in his vel asperitatibus rerum vel angustiis temporis obsequar studiis nostris et quantum mihi vel fraus inimicorum vel causae amicorum vel res publica tribuet oti, ad scribendum potissimum conferam; tibi vero, frater, neque hortanti deero neque roganti, nam neque auctoritate quisquam apud me plus valere te potest neque voluntate."""
+        self.cc_1 = """方廣錩〔題解〕《天竺國菩提達摩禪師論》，又名《達摩禪師論》，中國僧人假託禪宗初祖菩提達摩所撰典籍，著者不詳，一卷。在敦煌遺書中，"""
 
-        self.test_2 = """ac mihi repetenda est veteris cuiusdam memoriae non sane satis explicata recordatio, sed, ut arbitror, apta ad id, quod requiris, ut cognoscas quae viri omnium eloquentissimi clarissimique senserint de omni ratione dicendi. vis enim, ut mihi saepe dixisti, quoniam, quae pueris aut adulescentulis nobis ex commentariolis nostris incohata ac rudia exciderunt, vix sunt hac aetate digna et hoc usu, quem ex causis, quas diximus, tot tantisque consecuti sumus, aliquid eisdem de rebus politius a nobis perfectiusque proferri; solesque non numquam hac de re a me in disputationibus nostris dissentire, quod ego eruditissimorum hominum artibus eloquentiam contineri statuam, tu autem illam ab elegantia doctrinae segregandam putes et in quodam ingeni atque exercitationis genere ponendam. ac mihi quidem saepe numero in summos homines ac summis ingeniis praeditos intuenti quaerendum esse visum est quid esset cur plures in omnibus rebus quam in dicendo admirabiles exstitissent; nam quocumque te animo et cogitatione converteris, permultos excellentis in quoque genere videbis non mediocrium artium, sed prope maximarum. quis enim est qui, si clarorum hominum scientiam rerum gestarum vel utilitate vel magnitudine metiri velit, non anteponat oratori imperatorem? quis autem dubitet quin belli duces ex hac una civitate praestantissimos paene innumerabilis, in dicendo autem excellentis vix paucos proferre possimus? iam vero consilio ac sapientia qui regere ac gubernare rem publicam possint, multi nostra, plures patrum memoria atque etiam maiorum exstiterunt, cum boni perdiu nulli, vix autem singulis aetatibus singuli tolerabiles oratores invenirentur. ac ne qui forte cum aliis studiis, quae reconditis in artibus atque in quadam varietate litterarum versentur, magis hanc dicendi rationem, quam cum imperatoris laude aut cum boni senatoris prudentia comparandam putet, convertat animum ad ea ipsa artium genera circumspiciatque, qui in eis floruerint quamque multi sint; sic facillime, quanta oratorum sit et semper fuerit paucitas, iudicabit."""
+        self.cc_2 = """至今已經發現兩種題名為《達摩禪師論》的文獻。其一為日本橋本凝胤所藏，首殘尾存，尾題作「達摩禪師論」，係唐高宗開耀元年"""
 
-        self.latin_test_corpus = [self.test_1, self.test_2]
+        self.cc_test_corpus = [self.cc_1, self.cc_2]
 
-    def test_corpus_latin(self):
-        """Test production of Latin stoplists from a corpus"""
-        target_list = ['ac', 'atque', 'cum', 'et', 'in', 'mihi', 'neque',
-                        'qui', 'rerum', 'vel']
-        S = LatinCorpusStoplist()
-        stoplist = S.build_stoplist(self.latin_test_corpus, size=10,
-                    basis='zou', inc_values=False)
-
+    def test_corpus_stop_list_freq_classical_chinese(self):
+        """Test production of stoplists from a corpus, using basis: frequency"""
+        target_list = ['一', '中', '名', '國', '師', '摩', '禪', '論', '達', '題']
+        S = ClassicalChineseCorpusStoplist()
+        stoplist = S.build_stoplist(self.cc_test_corpus, size=10,
+                    basis='frequency', inc_values=False)
         self.assertEqual(stoplist, target_list)
 
 
